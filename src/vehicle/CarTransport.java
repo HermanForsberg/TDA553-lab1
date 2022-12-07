f0b9e43f16806d2f6c1d559d457a64d382af1e59
HermanForsberg <114741745+HermanForsberg@users.noreply.github.com>
2022-12-07 11:55:24 +0100

    fixed part of issue #20

--- src/vehicle/CarTransport.java
+++ src/vehicle/CarTransport.java
@@ -1,20 +1,17 @@
 package vehicle;
 import java.awt.*;
-import java.util.Stack;
 
 import flatbed.TwoStateFlatbed;
-import load.Load;
+import load.LoadStack;
 
-public class CarTransport extends Truck implements Load{
+public class CarTransport extends Truck {
     private TwoStateFlatbed flatbed;
-    private int maxNumberOfCarsLoaded;
-    private Stack<Car> carStack;
+    private LoadStack load;
 
     public CarTransport(int nrDoors, double enginePower, double currentSpeed, Color color, int maxNumberOfCarsLoaded){
         super(nrDoors, enginePower, currentSpeed, color); 
-        this.flatbed = new TwoStateFlatbed();   
-        this.maxNumberOfCarsLoaded = Math.max(maxNumberOfCarsLoaded, 1);  
-        this.carStack = new Stack<Car>();
+        this.flatbed = new TwoStateFlatbed(); 
+        this.load = new LoadStack(maxNumberOfCarsLoaded);
     }
 
     @Override
@@ -43,52 +40,21 @@ public class CarTransport extends Truck implements Load{
         }
     }
 
-    @Override
     public void load(Car car) { //You can only load cars.
-        if (canLoadCar(car)){
-            car.setLoaded(true);
-            this.carStack.push(car);
+        if (
+            (load.distanceToCar(car, getX(), getY()) < 1) && 
+            (!flatbed.getFlatbedUp())) {
+            load.load(car);
         }
     }
 
-    @Override
     public void unload(Car car) { //You can only load cars.
-        if (canUnloadCar(car)) {
-            carStack.pop();
-            car.setLoaded(false);
+        if ((flatbed.getFlatbedUp() == false)) {
+            load.unload(car);
         }
     }
 
-    private boolean canLoadCar(Car car) {
-        return (
-            (carStackNotFull(car)) && 
-            (distanceToCar(car) < 1) && 
-            (carNotMoving(car)) && 
-            (!flatbed.getFlatbedUp())
-        );
-    }
-    
-    private boolean canUnloadCar(Car car) {
-        return (
-            (!carStack.isEmpty()) && 
-            (carStack.peek() == car) && 
-            (flatbed.getFlatbedUp() == false)
-        );
-    }
 
-    private double distanceToCar(Car car) {
-        double distance;
-        double xDifference = car.getX() - getX();
-        double yDifference = car.getY() - getY();
-        distance = Math.sqrt(Math.pow(xDifference, 2) + Math.pow(yDifference, 2));
-        return distance;
-    }
 
-    private boolean carNotMoving(Car car) {
-        return (car.getDx() == 0) && (car.getDy() == 0);
-    }
 
-    private boolean carStackNotFull(Car car) {
-        return carStack.size() < maxNumberOfCarsLoaded;
-    }
 }
